f750b35b2ee0346d9ad169bb9ded622ac28cdb80
Rundll86 <2028673870@qq.com>
2024-06-16 12:55:33 +0800

    updated

--- install.js
+++ install.js
@@ -5,41 +5,36 @@ child_process.exec("py --version").addListener("exit", e => {
     if (e !== 0) {
         throw new Error("Cannot find python.");
     };
-    child_process.exec("python version_test.py").addListener("exit", e => {
+    child_process.exec("pip --version").addListener("exit", e => {
         if (e !== 0) {
-            throw new Error("Require python >= 3.9.7.");
+            throw new Error("Cannot find pip.");
         };
-        child_process.exec("pip --version").addListener("exit", e => {
-            if (e !== 0) {
-                throw new Error("Cannot find pip.");
+        let installProgress = child_process.exec("pip install pyinstaller");
+        installProgress.addListener("exit", e => {
+            if (e === 0) {
+                try { fs.rmSync("pyinstaller.exe") } catch { };
+                child_process.execSync(`copy "${child_process.execSync("where pyinstaller").toString().split("\n")[0]}" "${__dirname}"`);
+                child_process.spawn("pyinstaller", [
+                    "-F",
+                    "eop.py",
+                    "--add-data",
+                    "entry.pyw;.",
+                    "--add-data",
+                    "pyinstaller.exe;.",
+                    "--add-data",
+                    "favicon.ico;.",
+                    "--add-data",
+                    "build.spec;.",
+                    "--add-data",
+                    "package.json;.",
+                    "--add-data",
+                    "presets;presets"
+                ]).addListener("exit", e => {
+                    if (e !== 0) { throw new Error("Failed to generate builder CLI."); };
+                });
+            } else {
+                throw new Error("Failed to install pyinstaller.");
             };
-            let installProgress = child_process.exec("pip install pyinstaller");
-            installProgress.addListener("exit", e => {
-                if (e === 0) {
-                    try { fs.rmSync("pyinstaller.exe") } catch { };
-                    child_process.execSync(`copy "${child_process.execSync("where pyinstaller").toString().split("\n")[0]}" "${__dirname}"`);
-                    child_process.spawn("pyinstaller", [
-                        "-F",
-                        "eop.py",
-                        "--add-data",
-                        "entry.pyw;.",
-                        "--add-data",
-                        "pyinstaller.exe;.",
-                        "--add-data",
-                        "favicon.ico;.",
-                        "--add-data",
-                        "build.spec;.",
-                        "--add-data",
-                        "package.json;.",
-                        "--add-data",
-                        "presets;presets"
-                    ]).addListener("exit", e => {
-                        if (e !== 0) { throw new Error("Failed to generate builder CLI."); };
-                    });
-                } else {
-                    throw new Error("Failed to install pyinstaller.");
-                };
-            });
         });
     });
 });
