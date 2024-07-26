2352067ee88636ce5954b0c5dd8cae9c8754bbb9
Rundll86 <2028673870@qq.com>
2024-07-26 15:50:26 +0800

    updated

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
 echo off
 cls
-push.bat && npm version patch && npm publish && npm update electron-ofp -g
+npm config set registry=https://registry.npmjs.org && push.bat && npm version patch && npm publish && npm update electron-ofp -g
